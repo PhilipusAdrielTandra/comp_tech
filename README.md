cc97f02b5ec60966db8b6958b9a81badea1ba4bb
CH1MP5T0N <philipusadriel@gmail.com>
2023-11-06 18:41:17 +0700

    documentation and such

---
+++ README.md
@@ -0,0 +1,41 @@
+# Recursive Calculator
+Good afternoon sir, `main.py` contains the recursive calculator and is able to allow white space between numbers as well as account for parse tree construction, has division and contains early error detection.
+
+## White space
+```
+user_input = list(input('> ').replace(' ', ''))
+```
+This statement in the program removes whitespace from the inputs and allows for calculations
+
+## Parse Tree construction
+### Example Ouput
+```
+> 6 / 3 + 5
+Parse Tree:
++
+  /
+    6.0
+    3.0
+  5.0
+```
+This is example output of the program for parse tree construction
+
+## Additional operators
+### Example Output
+```
+> 6/ 2
+Parse Tree:
+/
+  6.0
+  2.0
+Result: 3.0
+```
+Here is example output for division
+
+## Early Error detection
+### Example Output
+```
+> $ + 
+Error: Invalid input token: $
+```
+This is an example of when there is an invalid input token
--- main.py
+++ main.py
@@ -1,44 +1,73 @@
+class ParseTreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
 class RecursiveCalculator:
     def __init__(self, input_tokens):
         self._tokens = input_tokens
         self._current_token = input_tokens[0]
 
     def evaluate_expression(self):
-        result = self.calculate_term()
+        result, parse_tree = self.calculate_term()
         while self._current_token in ('+', '-'):
-            if self._current_token == '+':
-                self.advance()
-                result += self.calculate_term()
-            elif self._current_token == '-':
-                self.advance()
-                result -= self.calculate_term()
-        return result
+            operator = self._current_token
+            self.advance()
+            term_value, term_tree = self.calculate_term()
+            if operator == '+':
+                result += term_value
+                parse_tree = ParseTreeNode("+", parse_tree, term_tree)
+            elif operator == '-':
+                result -= term_value
+                parse_tree = ParseTreeNode("-", parse_tree, term_tree)
+        return result, parse_tree
 
     def calculate_factor(self):
-        result = None
+        result, parse_tree = None, None
+        if self._current_token is None:
+            raise ValueError("Unexpected end of input")
         if self._current_token[0].isdigit() or self._current_token[-1].isdigit():
             result = float(self._current_token)
+            parse_tree = ParseTreeNode(result)
             self.advance()
         elif self._current_token == '(':
             self.advance()
-            result = self.evaluate_expression()
+            result, parse_tree = self.evaluate_expression()
+            if self._current_token != ')':
+                raise ValueError("Expected closing parenthesis ')'")
             self.advance()
-        return result
+        else:
+            raise ValueError("Invalid input token: " + self._current_token)
+        return result, parse_tree
 
     def advance(self):
         self._tokens = self._tokens[1:]
         self._current_token = self._tokens[0] if len(self._tokens) > 0 else None
 
     def calculate_term(self):
-        result = self.calculate_factor()
+        result, parse_tree = self.calculate_factor()
         while self._current_token in ('*', '/'):
-            if self._current_token == '*':
-                self.advance()
-                result *= self.calculate_factor()
-            elif self._current_token == '/':
-                self.advance()
-                result /= self.calculate_factor()
-        return result
+            operator = self._current_token
+            self.advance()
+            factor_value, factor_tree = self.calculate_factor()
+            if operator == '*':
+                result *= factor_value
+                parse_tree = ParseTreeNode("*", parse_tree, factor_tree)
+            elif operator == '/':
+                if factor_value == 0:
+                    raise ValueError("Division by zero is not allowed")
+                result /= factor_value
+                parse_tree = ParseTreeNode("/", parse_tree, factor_tree)
+        return result, parse_tree
+
+    def print_parse_tree(self, node, indent=""):
+        if node is not None:
+            print(indent + str(node.value))
+            if node.left:
+                self.print_parse_tree(node.left, indent + "  ")
+            if node.right:
+                self.print_parse_tree(node.right, indent + "  ")
 
 if __name__ == '__main__':
     while True:
@@ -51,4 +80,12 @@ if __name__ == '__main__':
                 input_tokens[-1] += user_input[i]
             else:
                 input_tokens.append(user_input[i])
-        print(RecursiveCalculator(input_tokens).evaluate_expression())
+        
+        try:
+            calculator = RecursiveCalculator(input_tokens)
+            result, parse_tree = calculator.evaluate_expression()
+            print("Parse Tree:")
+            calculator.print_parse_tree(parse_tree)
+            print("Result:", result)
+        except ValueError as e:
+            print("Error:", e)
